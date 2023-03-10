1ee6b3555828fdd8a919137dc4198c28607e5567
wer8888 <91093536+wer8888@users.noreply.github.com>
2023-03-10 17:27:20 -0500

    Set up SFML

--- chip-8-emulator/src/emulator.cpp
+++ chip-8-emulator/src/emulator.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	std::cout << "Hello, world!" << std::endl;
+    sf::RenderWindow window(sf::VideoMode(720, 480), "Game", sf::Style::Close | sf::Style::Titlebar);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+
+        // Draw
+
+        window.display();
+    }
+
+    return 0;
 }
